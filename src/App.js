e86da0eb90c335b4617a2f834eacdf990a09a91a
murshidazher <murshidazher@yahoo.com>
2020-06-17 13:18:37 +0530

    feat(hello): add basic react template

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <h1>hello world</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
